d052ed911589795f4af19ff0112ed7d00c96901c
ghadj <georgehadjiantonis@hotmail.com>
2021-06-15 18:21:51 +0300

    add hed batch processing

---
+++ hed/batch_hed.py
@@ -0,0 +1,22 @@
+import os
+import cv2 as cv
+
+import edge_detector as ed
+
+SRC_PATH = '../Dataset/train/'
+DEST_PATH =  '../Dataset/test/'
+
+img_extentions = ['.jpg', '.png']
+
+detector = ed.EdgeDetector()
+
+files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
+    os.path.expanduser(SRC_PATH)) for f in fn if f.endswith(tuple(img_extentions))]
+
+for f in files:
+    path, filename = os.path.split(f)
+    f_hed = DEST_PATH + filename[:-4] + '_hed' + filename[-4:]
+    print(f + ' => ' + f_hed)
+    input = cv.imread(f)
+    output = detector.run(input)
+    cv.imwrite(f_hed, output)
