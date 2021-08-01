1b868dab059c48d50411cbe5f0e0dbb6db5c59fd
ghadj <georgehadjiantonis@hotmail.com>
2021-08-01 23:36:43 +0300

    update params

--- pix2pix/params.py
+++ pix2pix/params.py
@@ -8,15 +8,18 @@ OUTPUT_CHANNELS = 3
 LAMBDA = 100
 
 # Image
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+IMG_WIDTH = 1024
+IMG_HEIGHT = 1024
 
 # Train
+BRIGHTNESS_DELTA = 0.2 # max brightness delta
 EPOCHS = 300
-BUFFER_SIZE = 400
+BUFFER_SIZE = 20000 # greater than or equal to the full size of the dataset
 BATCH_SIZE = 1
-DATASET_PATH = os.path.join(package_dir, '..', 'Dataset')
+TRAIN_DATASET_PATH = os.path.join(package_dir, '..', 'Dataset', 'train', '*.jpg')
+TEST_DATASET_PATH = os.path.join(package_dir, '..', 'Dataset', 'test', '*.jpg')
 LOG_DIR = os.path.join(package_dir, 'logs', 'fit')
 CHECKPOINT_DIR = os.path.join(package_dir, 'training_checkpoints')
 CHECKPOINT_IMG_DIR = os.path.join(package_dir, CHECKPOINT_DIR, 'sample-output')
 CONTINUE_FROM_LAST_CHECK = True
+CHECKPOINT_EVERY_EPOCH = 1
